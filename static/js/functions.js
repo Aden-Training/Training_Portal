1407486a30f08411f7f4575fe4f69b2dc64f6820
theRGTgamer <40317736@live.napier.ac.uk>
2020-02-08 13:41:35 +0000

    update to bootstrap 4

---
+++ static/js/functions.js
@@ -0,0 +1,9 @@
+function switchVisibleHGV(){
+    document.getElementById('divOffshore').style.display = 'none';
+    document.getElementById('divHGV').style.display = 'block';
+}
+
+function switchVisibleOffshore(){
+    document.getElementById('divHGV').style.display = 'none';
+    document.getElementById('divOffshore').style.display = 'block';
+}
