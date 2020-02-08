072758a426b4e06df85b973e30ad5654a2aad5ff
theRGTgamer <40317736@live.napier.ac.uk>
2020-02-08 13:25:42 +0000

    Should have fixed master

--- app.py
+++ app.py
@@ -1,16 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-=======
-<<<<<<< HEAD
->>>>>>> tyler
->>>>>>> master
-=======
-=======
-<<<<<<< HEAD
->>>>>>> tyler
->>>>>>> master
 from flask import Flask, render_template, url_for, request, redirect, session, flash
 from functools import wraps
 import sqlite3
@@ -190,49 +177,38 @@ def loginbus():
 
 
 if __name__ == "__main__":
-<<<<<<< HEAD
     app.run(host="0.0.0.0", debug=True)
-=======
-    app.run(host="0.0.0.0", debug=True)
-=======
-from flask import Flask, render_template, request, redirect, url_for, session
-import smtplib, ssl
-from email.mime.text import MIMEText
-from email.mime.multipart import MIMEMultipart
 
-app = Flask(__name__)
+# from flask import Flask, render_template, request, redirect, url_for, session
+# import smtplib, ssl
+# from email.mime.text import MIMEText
+# from email.mime.multipart import MIMEMultipart
 
-@app.route('/')
-def root():
-    return render_template("index.html")
+# app = Flask(__name__)
+
+# @app.route('/')
+# def root():
+#     return render_template("index.html")
 
 
-@app.route('/sendEmail', methods=["GET", "POST"])
-def sendEmail():
-    if request.method == "POST":
-        emailAd = request.form['emailAddress']
-        courseT = request.form['courseType']
-        reqDay = request.form['requestedDay']
+# @app.route('/sendEmail', methods=["GET", "POST"])
+# def sendEmail():
+#     if request.method == "POST":
+#         emailAd = request.form['emailAddress']
+#         courseT = request.form['courseType']
+#         reqDay = request.form['requestedDay']
 
-        makeEmail(emailAd, courseT, reqDay)
+#         makeEmail(emailAd, courseT, reqDay)
 
-    return render_template("new.html")
+#     return render_template("new.html")
 
 
-def makeEmail(recEmail, courseT, reqDay):
-    #Add the shit pls ross
+# def makeEmail(recEmail, courseT, reqDay):
+#     #Add the shit pls ross
 
 
 
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=True)
->>>>>>> master
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> tyler
->>>>>>> master
-=======
->>>>>>> tyler
->>>>>>> master
+# if __name__ == "__main__":
+#     app.run(host='0.0.0.0', debug=True)
+
