79fd4d20e6fbd0d745b56a36ca4b448ebfad91a5
theRGTgamer <40317736@live.napier.ac.uk>
2020-02-12 11:10:26 +0000

    working on search bar

--- static/js/functions.js
+++ static/js/functions.js
@@ -7,3 +7,21 @@ function switchVisibleOffshore(){
     document.getElementById('divHGV').style.display = 'none';
     document.getElementById('divOffshore').style.display = 'block';
 }
+
+function searchCourses(){
+    let search = document.getElementById('searchBar').value
+    search = search.toLowerCase();
+    let x = document.getElementsByClassName('card')
+    let y = document.getElementsByClassName('card-title');
+
+    document.getElementById('divHGV').style.display = 'none';
+    document.getElementById('divOffshore').style.display = 'none';
+
+    for(i = 0; i < x.length; i++){
+        if(!y[i].innerHTML.toLowerCase().includes(search)){
+            x[i].style.display="none";
+        }else{
+            x[i].style.display="inline-block";
+        }
+    }
+}
