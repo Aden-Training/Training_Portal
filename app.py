a891ff713b3faf5e52dc3eb6593839aeaab5f0c8
Handley <40326169@soc-web-liv-81.napier.ac.uk>
2020-02-12 16:55:41 +0000

    Adding individual user database

--- app.py
+++ app.py
@@ -3,44 +3,53 @@ from functools import wraps
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 
-import sqlite3
-import bcrypt
+#import sqlite3
+#import bcrypt
 import random
 import smtplib, ssl
 
 app = Flask(__name__)
-app.secret_key = "lmaosecretkeylmao"
+#app.secret_key = "lmaosecretkeylmao"
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = '123456'
+app.config['MYSQL_DB'] = 'adentraining'
+app.config['MYSQL_DB'] = 'DictCursor'
 
-#   Authentication Middleware
-def requires_login(f):
-    @wraps(f)
-    def decorated(*args, **kwargs):
-        status = session.get('logged_in', False)
-        busstatus = session.get('bus_logged_in', False)
-        if not status:
-            return redirect(url_for('.login', next=request.path))
-        return f(*args, **kwargs)
-    return decorated
+#init MYSQL
+mysql= MySQL(app)
 
-def requires_bus_login(f):
-    @wraps(f)
-    def decorated(*args, **kwargs):
-        busstatus = session.get('bus_logged_in', False)
-        if not busstatus:
-            return redirect(url_for('.loginbus', next=request.path))
-        return f(*args, **kwargs)
-    return decorated
 
-conn = sqlite3.connect("db/database.db")
+# Authentication Middleware
+#def requires_login(f):
+    #@wraps(f)
+    #def decorated(*args, **kwargs):
+     #   status = session.get('logged_in', False)
+      #  busstatus = session.get('bus_logged_in', False)
+       # if not status:
+        #    return redirect(url_for('.login', next=request.path))
+       # return f(*args, **kwargs)
+   # return decorated
 
-conn.execute("CREATE TABLE IF NOT EXISTS customers (email TEXT, username TEXT, password TEXT)")
-conn.execute("CREATE TABLE IF NOT EXISTS businesses (email TEXT, username TEXT, password TEXT, industry TEXT)")
-conn.execute("CREATE TABLE IF NOT EXISTS courses (course_name TEXT, description TEXT, catagory TEXT, thumbnail TEXT)")
-conn.execute("CREATE TABLE IF NOT EXISTS bookings (course_name TEXT, person_booked TEXT, persons_email TEXT)")
+#def requires_bus_login(f):
+ #   @wraps(f)
+  #  def decorated(*args, **kwargs):
+   #     busstatus = session.get('bus_logged_in', False)
+    #    if not busstatus:
+     #       return redirect(url_for('.loginbus', next=request.path))
+      #  return f(*args, **kwargs)
+   # return decorated
+
+#conn = sqlite3.connect("db/database.db")
+
+#conn.execute("CREATE TABLE IF NOT EXISTS customers (email TEXT, username TEXT, password TEXT)")
+#conn.execute("CREATE TABLE IF NOT EXISTS businesses (email TEXT, username TEXT, password TEXT, industry TEXT)")
+#conn.execute("CREATE TABLE IF NOT EXISTS courses (course_name TEXT, description TEXT, catagory TEXT, thumbnail TEXT)")
+#conn.execute("CREATE TABLE IF NOT EXISTS bookings (course_name TEXT, person_booked TEXT, persons_email TEXT)")
 
 #   CUSTOMER PAGES
 
-@app.route('/')
+#@app.route('/')
 @requires_login
 def root():
     return render_template("index.html")
@@ -51,10 +60,10 @@ def logout():
 
 #   BUSINESS PAGES
 
-@app.route('/businesstraining')
-@requires_bus_login
-def bustraining():
-    return render_template('businessTraining.html')
+#@app.route('/businesstraining')
+#@requires_bus_login
+#def bustraining():
+ #   return render_template('businessTraining.html')
 
 #   COURSE HANDLING
 
@@ -71,160 +80,161 @@ def bustraining():
     
 #     return redirect('/businesstraining')
 
-@app.route('/postcourses', methods = ["POST","GET"])
-def postcourses():
-    if request.method == "POST":
+#@app.route('/postcourses', methods = ["POST","GET"])
+#def postcourses():
+ #   if request.method == "POST":
 
-        f = request.files['imageFile']
-        name = request.form['courseTitle']
-        desc = request.form['courseDescription']
-        cat = request.form['courseCat']
+  #      f = request.files['imageFile']
+   #     name = request.form['courseTitle']
+    #    desc = request.form['courseDescription']
+     #   cat = request.form['courseCat']
         #thumb = request.form['imageFile']
 
-        path = 'static/img/' + name + '.jpg'
-        f.save('static/img/' + name +'.jpg')
+     #   path = 'static/img/' + name + '.jpg'
+      #  f.save('static/img/' + name +'.jpg')
 
-        with sqlite3.connect('db/database.db') as con:
-            con.execute("INSERT INTO courses VALUES (?,?,?,?)", (name, desc, cat, path))
+       # with sqlite3.connect('db/database.db') as con:
+        #    con.execute("INSERT INTO courses VALUES (?,?,?,?)", (name, desc, cat, path))
     
-        return redirect('/findcourse')
+       # return redirect('/findcourse')
 
-    return render_template('postcourse.html')
+    #return render_template('postcourse.html')
 
-@app.route('/courses', methods=["GET"])
-def courses():
-    con = sqlite3.connect('db/database.db')
-    con.row_factory = sqlite3.Row
+#@app.route('/courses', methods=["GET"])
+#def courses():
+ #   con = sqlite3.connect('db/database.db')
+  #  con.row_factory = sqlite3.Row
 
-    cur = con.cursor()
+   # cur = con.cursor()
 
-    catagory = "Offshore"
+    #catagory = "Offshore"
 
-    cur.execute("SELECT * FROM courses WHERE catagory = ?",[catagory])
+    #cur.execute("SELECT * FROM courses WHERE catagory = ?",[catagory])
 
-    courses = cur.fetchall()
+    #courses = cur.fetchall()
 
-    return render_template('courses.html', courses = courses)
+   # return render_template('courses.html', courses = courses)
 
 #   LOGGING IN CUSTOMERS
 
-@app.route('/coursesavailable', methods=["GET"])
-def coursesavailable():
-    con = sqlite3.connect('db/database.db')
-    con.row_factory = sqlite3.Row
+#@app.route('/coursesavailable', methods=["GET"])
+#def coursesavailable():
+ #   con = sqlite3.connect('db/database.db')
+  #  con.row_factory = sqlite3.Row
 
-    cur = con.cursor()
+   # cur = con.cursor()
 
-    cur.execute("SELECT * FROM courses")
-    courses = cur.fetchall()
+    #cur.execute("SELECT * FROM courses")
+    #courses = cur.fetchall()
 
-    return render_template('coursesavailable.html', courses = courses)
+    #return render_template('coursesavailable.html', courses = courses)
 
 
-@app.route('/registercust', methods = ["GET","POST"])
-def register():
-    if request.method == "POST":
-        with sqlite3.connect("db/database.db") as con:
-            email = request.form['email']
-            username = request.form['username']
-            password = request.form['password']
+#@app.route('/registercust', methods = ["GET","POST"])
+#def register():
+ #   if request.method == "POST":
+  #      with sqlite3.connect("db/database.db") as con:
+   #         email = request.form['email']
+     #       username = request.form['username']
+    #        password = request.form['password']
 
-            passwd = password.encode('utf-8')
-            hashedpw = bcrypt.hashpw(passwd, bcrypt.gensalt())
+      #      passwd = password.encode('utf-8')
+       #     hashedpw = bcrypt.hashpw(passwd, bcrypt.gensalt())
 
-            con.execute("INSERT INTO customers VALUES(?,?,?)", (email, username, hashedpw))
+        #    con.execute("INSERT INTO customers VALUES(?,?,?)", (email, username, hashedpw))
 
-            status = session['logged_in'] = True
-            session['user'] = request.form['username']
+         #   status = session['logged_in'] = True
+          #  session['user'] = request.form['username']
 
-            return redirect('/')
+           # return redirect('/')
 
-    return render_template("register.html")
+   # return render_template("register.html")
 
-@app.route('/logincust', methods = ["GET","POST"])
-def login():
-    if request.method == "POST":
+#@app.route('/logincust', methods = ["GET","POST"])
+#def login():
+ #   if request.method == "POST":
 
-        username = request.form['username']
-        password = request.form['password']
+  #      username = request.form['username']
+ #       password = request.form['password']
 
-        encodedpw = password.encode('utf-8')
+  #      encodedpw = password.encode('utf-8')
 
-        with sqlite3.connect("db/database.db") as con:
-            cur = con.cursor()
-            cur = con.execute("SELECT * FROM customers WHERE username = ?", [username])
+    #    with sqlite3.connect("db/database.db") as con:
+      #      cur = con.cursor()
+        #    cur = con.execute("SELECT * FROM customers WHERE username = ?", [username])
 
-            user = cur.fetchone()
+         #   user = cur.fetchone()
 
-            if cur != "":
-                passwd = user[1]
+          #  if cur != "":
+           #     passwd = user[1]
 
-                if(bcrypt.checkpw(encodedpw, passwd)):
-                    status = session['logged_in'] = True
-                    session['user'] = request.form['username']
-                    return redirect('/')
-    return render_template("login.html")
+            #    if(bcrypt.checkpw(encodedpw, passwd)):
+             #       status = session['logged_in'] = True
+              #      session['user'] = request.form['username']
+               #     return redirect('/')
+   # return render_template("login.html")
 
 #   LOGGING IN businesses
-@app.route('/registerbus', methods = ["GET","POST"])
-def registerbus():
-    if request.method == "POST":
-        email = request.form['email']
-        username = request.form['username']
-        password = request.form['password']
-        industry = request.form['industry']
+#@app.route('/registerbus', methods = ["GET","POST"])
+#def registerbus():
+ #   if request.method == "POST":
+  #      email = request.form['email']
+   #     username = request.form['username']
+    #    password = request.form['password']
+     #   industry = request.form['industry']
 
-        passwd = password.encode('utf-8')
+      #  passwd = password.encode('utf-8')
 
-        hashpw = bcrypt.hashpw(passwd, bcrypt.gensalt())
+       # hashpw = bcrypt.hashpw(passwd, bcrypt.gensalt())
 
-        with sqlite3.connect("db/database.db") as con:
-            con.execute("INSERT INTO businesses VALUES(?,?,?,?)",(email,username,hashpw,industry))
+        #with sqlite3.connect("db/database.db") as con:
+         #   con.execute("INSERT INTO businesses VALUES(?,?,?,?)",(email,username,hashpw,industry))
 
-            busstatus = session['bus_logged_in'] = True
-            session['user'] = request.form['username']
+          #  busstatus = session['bus_logged_in'] = True
+           # session['user'] = request.form['username']
 
-            return redirect('/businesstraining')
+            #return redirect('/businesstraining')
 
-    return render_template("registerbus.html")
+    #return render_template("registerbus.html")
 
-@app.route('/loginbus', methods = ["GET","POST"])
-def loginbus():
-    if request.method == "POST":
-        username = request.form['username']
-        password = request.form['password']
+#@app.route('/loginbus', methods = ["GET","POST"])
+#def loginbus():
+ #   if request.method == "POST":
+   #     username = request.form['username']
+  #      password = request.form['password']
 
-        encodedpw = password.encode('utf-8')
+    
+#    encodedpw = password.encode('utf-8')
+#
+ #       with sqlite3.connect("db/database.db") as con:
+  #          cur = con.cursor()
+   #         cur = con.execute("SELECT * FROM businesses WHERE username = ?", [username])
 
-        with sqlite3.connect("db/database.db") as con:
-            cur = con.cursor()
-            cur = con.execute("SELECT * FROM businesses WHERE username = ?", [username])
+    #        user = cur.fetchone()
 
-            user = cur.fetchone()
+     #       if cur != "":
+      #          passwd = user[1]
 
-            if cur != "":
-                passwd = user[1]
+       #         if(bcrypt.checkpw(encodedpw, passwd)):
+         #           busstatus = session['bus_logged_in'] = True
+        #            session['user'] = request.form['username']
+          #          return redirect('/businesstraining')
 
-                if(bcrypt.checkpw(encodedpw, passwd)):
-                    busstatus = session['bus_logged_in'] = True
-                    session['user'] = request.form['username']
-                    return redirect('/businesstraining')
+    #return render_template('loginbus.html')
 
-    return render_template('loginbus.html')
+#@app.route('/findcourse', methods=["GET", "POST"])
+#@requires_login
+#def findcourse():
+ #   con = sqlite3.connect('db/database.db')
+  #  con.row_factory = sqlite3.Row
 
-@app.route('/findcourse', methods=["GET", "POST"])
-@requires_login
-def findcourse():
-    con = sqlite3.connect('db/database.db')
-    con.row_factory = sqlite3.Row
+    #cur = con.cursor()
+   # catagory = "Offshore"
 
-    cur = con.cursor()
-    catagory = "Offshore"
+    #cur.execute("SELECT * FROM courses WHERE catagory = ?",[catagory])
+    #course = cur.fetchall()
 
-    cur.execute("SELECT * FROM courses WHERE catagory = ?",[catagory])
-    course = cur.fetchall()
-
-    return render_template('findcourse.html', course = course)
+    #return render_template('findcourse.html', course = course)
 
 # #Ross's stuff, can remove later
 # @app.route('/findcourse', methods=["GET", "POST"])
@@ -235,51 +245,51 @@ def findcourse():
 #     cur = con.cursor()
 #     catagory = "Offshore"
 
-@app.route('/removecourse/<id>', methods=["GET", "POST"])
-def removecourse(id):
-    if request.method == "POST":
-        with sqlite3.connect('db/database.db') as con:
-            con.execute("DELETE FROM courses WHERE id = ?", [id])
-            con.commit()
+#@app.route('/removecourse/<id>', methods=["GET", "POST"])
+#def removecourse(id):
+ #   if request.method == "POST":
+  #      with sqlite3.connect('db/database.db') as con:
+   #         con.execute("DELETE FROM courses WHERE id = ?", [id])
+    #        con.commit()
 
-        return redirect('/courses')
+     #   return redirect('/courses')
 
 # Creating an alternative bookcourse method
 
-@app.route('/bookcourse/<coursename>', methods=["POST","GET"])
-def bookcourse(coursename):
-    if request.method == "POST":
-        con = sqlite3.connect('db/database.db')
-        cur = con.cursor()
+#@app.route('/bookcourse/<coursename>', methods=["POST","GET"])
+#def bookcourse(coursename):
+ #   if request.method == "POST":
+  #      con = sqlite3.connect('db/database.db')
+   #     cur = con.cursor()
 
-        course = coursename
-        user = session['user']
+    #    course = coursename
+     #   user = session['user']
 
-        cur.execute("SELECT * FROM customers WHERE username = ?",[user])
+      #  cur.execute("SELECT * FROM customers WHERE username = ?",[user])
 
-        cust_data = cur.fetchone()
+       # cust_data = cur.fetchone()
 
-        email = cust_data[0]
+        #email = cust_data[0]
 
-        with sqlite3.connect('db/database.db') as con:
-            con.execute("INSERT INTO bookings VALUES (?,?,?)",(course,user,email))
-            con.commit()
+        #with sqlite3.connect('db/database.db') as con:
+         #   con.execute("INSERT INTO bookings VALUES (?,?,?)",(course,user,email))
+          #  con.commit()
 
-    flash("You're Successfully booked onto %s!" % course)
+   # flash("You're Successfully booked onto %s!" % course)
 
-    return redirect('/findcourse')
+   # return redirect('/findcourse')
 
 
-@app.route('/peoplebooked/<coursename>', methods=["GET"])
-def peoplebooked(coursename):
-    con = sqlite3.connect('db/database.db')
-    cur = con.cursor()
+#@app.route('/peoplebooked/<coursename>', methods=["GET"])
+#def peoplebooked(coursename):
+ #   con = sqlite3.connect('db/database.db')
+  #  cur = con.cursor()
 
-    cur.execute("SELECT person_booked FROM bookings WHERE course_name = ?", [coursename])
+   # cur.execute("SELECT person_booked FROM bookings WHERE course_name = ?", [coursename])
 
-    people = cur.fetchall()
+    #people = cur.fetchall()
 
-    return render_template('peoplebooked.html', people = people)
+    #return render_template('peoplebooked.html', people = people)
 
 
 
