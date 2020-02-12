a83c03977fb04e18945eb0ea7cf80baa4b6e2ff3
theRGTgamer <40317736@live.napier.ac.uk>
2020-02-12 14:04:29 +0000

    Problems with the search bar but mostly done

--- static/js/functions.js
+++ static/js/functions.js
@@ -9,19 +9,20 @@ function switchVisibleOffshore(){
 }
 
 function searchCourses(){
+    
+    // document.getElementById('divHGV').style.display = 'none';
+    // document.getElementById('divOffshore').style.display = 'none';
+
     let search = document.getElementById('searchBar').value
     search = search.toLowerCase();
     let x = document.getElementsByClassName('card')
     let y = document.getElementsByClassName('card-title');
 
-    document.getElementById('divHGV').style.display = 'none';
-    document.getElementById('divOffshore').style.display = 'none';
-
     for(i = 0; i < x.length; i++){
         if(!y[i].innerHTML.toLowerCase().includes(search)){
             x[i].style.display="none";
         }else{
-            x[i].style.display="inline-block";
+            x[i].style.display="block";
         }
     }
 }
