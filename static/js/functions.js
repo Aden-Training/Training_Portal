4f09f2f062230ab59718b74bc110d3eff3f23d48
alexander.racey@gmail.com <alexander.racey@gmail.com>
2020-03-03 16:09:45 +0000

    login page done, needs work on window resizing

--- static/js/functions.js
+++ static/js/functions.js
@@ -9,11 +9,15 @@ function switchVisibleOffshore(){
 }
 
 // Resizes navigation bar if screen is less that 600px
-function navbarChange() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
+function navbarChange()
+{
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") 
+  {
+    x.className += " responsive";
+  } 
+  else
+  {
+    x.className = "topnav";
   }
+}
