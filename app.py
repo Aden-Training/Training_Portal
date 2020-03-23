6d1d7f817593f9c84ae022b004ffb79bcec519f4
Emily <40310983@live.napier.ac.uk>
2020-03-23 13:13:51 +0000

    changed back

--- app.py
+++ app.py
@@ -159,7 +159,7 @@ def findcourse():
     cur = con.cursor()
     catagory = "Offshore"
 
-    cur.execute("SELECT * FROM courses WHERE catagory = ?")
+    cur.execute("SELECT * FROM courses WHERE catagory = ?",[catagory])
     course = cur.fetchall()
 
     return render_template('findcourse.html', course = course)
